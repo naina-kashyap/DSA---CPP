ebdd4022406fe2c4b4f7e4ad96d3e92151ac6491
naina-kashyap <nk95488nainakashyap@gmail.com>
2026-01-26 13:50:43 +0530

    missing_num_XOR_method.cpp

---
+++ Step 02 - arrays/missing_num_XOR_method.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing (vector<int> &a, int N){                                        
+    int xor1=0;
+    int xor2=0;
+    int n =N-1;
+    for(int i=0;i<n;i++){
+        xor2=xor2^a[i];
+        xor1=xor1^(i+1);
+    }
+    xor1=xor1^ N;
+    return xor1^xor2;
+}
+int main(){
+    vector<int>a={1,2,3,5};
+    cout<<missing(a,5);
+    return 0;
+}
