d2308ddeb3f4827e8d0514edd8999abf2c1ed9e0
naina-kashyap <nk95488nainakashyap@gmail.com>
2026-01-27 17:25:43 +0530

    maximum_consecutiveness.cpp

---
+++ Step 02 - arrays/maximum_consecutiveness.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findmax(vector<int>&a, int n){                                            
+    int maxi=0;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==1){
+            cnt++;
+            maxi=max(maxi,cnt);
+        }
+        else{
+            cnt=0;
+        }
+    }
+    return maxi;
+}
+int main (){
+    vector<int> a={1,1,2,1,1,1,0,1,1};
+    cout<<findmax(a,9);
+    return 0;
+}
